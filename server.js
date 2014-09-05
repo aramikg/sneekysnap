17edce11694759202376af3986f0ac7c18f9e29f
aramik <aramik@whalerockindustries.com>
2014-09-04 17:40:50 -0700

    added server.js

---
+++ server.js
@@ -0,0 +1,55 @@
+var app = require('express')();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+var express = require('express');
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var swig = require('swig');
+var path = require('path');
+var mongodb = require('mongodb');
+
+if (process.env.NODE_ENV == "development") {
+	server.listen(8080);
+} else {
+	server.listen(80);
+}
+app.engine('html',swig.renderFile);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'html');
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(cookieParser());
+app.use(bodyParser());
+
+
+/* ROUTES */
+//var createApp = require('./routes/user-apps');
+
+	app.get('/', function (req, res) {
+		res.render('index');
+	});
+
+
+
+/* SOCKET STUFF */
+io.on('connection', function (socket) {
+	console.log(socket.id + ' connected!');
+	var world = socket.handshake.headers.host;
+
+	var db = new mongodb.Db(world, new mongodb.Server('127.0.0.1', 27017), {safe:true});
+
+
+  socket.join(world); //join domain room
+
+	socket.on('news',function(data) {
+		console.log(socket);
+		io.to(world).emit('news',data);
+		console.log('MESSAGE SENT TO EVERYONE AT ' + world);
+	});
+
+  socket.on('disconnect',function(data) {
+  	console.log(socket.id + ' disconnected!');
+  });
+
+
+
+});
