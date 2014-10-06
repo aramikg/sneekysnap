46cbd1ba1c0107f67eaec01bfa8df33f1029cc22
aramik <aramik@whalerockindustries.com>
2014-10-06 10:47:50 -0700

    updated error message

--- routes/api.js
+++ routes/api.js
@@ -86,7 +86,7 @@ router.get('/feed/local', function(req,res,next){
 		                    if (filteredResults.length > 0) {
 		                    	res.send(filteredResults);
 		                    } else {
-		                    	res.send("{\"error\":\"no local results\"");
+		                    	res.send("{\"error\":\"no local results\"}");
 		                    }
 
 		                    db.close();
